55d8c0013334c5aecf06cc4f2e34b7d820fd0f2b
TadaMatz <matz@west.sd.keio.ac.jp>
2017-12-22 00:14:56 +0900

    create new version using led

---
+++ version3/arduino/mote/mote.ino
@@ -0,0 +1,17 @@
+#define LED_RED_PIN 3
+#define LED_GREEN_PIN 5
+#define LED_BLUE_PIN 6
+
+#define BUTTON_RED_PIN 10
+#define BUTTON_GREEN_PIN 9
+#define BUTTON_BLUE_PIN 8
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
