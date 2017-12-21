8d5227819b9e750e8a58341204883d337a5f650c
TadaMatz <matz@west.sd.keio.ac.jp>
2017-12-22 00:46:30 +0900

    simple model created

--- version3/arduino/mote/mote.ino
+++ version3/arduino/mote/mote.ino
@@ -6,12 +6,55 @@
 #define BUTTON_GREEN_PIN 9
 #define BUTTON_BLUE_PIN 8
 
-void setup() {
-  // put your setup code here, to run once:
+int oldR = 0;
+int oldG = 0;
+int oldB = 0;
 
+#define CNT_MAX 2
+int cntR = 0;
+int cntG = 0;
+int cntB = 0;
+
+int clickDetection(int _pin, int * _oldstate) { //click detection
+  int buttonClicked = 0;
+  int newButtonState = digitalRead(_pin);
+  if (*_oldstate == HIGH && newButtonState == LOW) buttonClicked = 1; //button pressed. CAUTION pull up
+  *_oldstate = newButtonState;
+  return buttonClicked;
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_BLUE_PIN, OUTPUT);
+  pinMode(BUTTON_RED_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_GREEN_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_BLUE_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (clickDetection(BUTTON_RED_PIN, &oldR)) {
+    cntR = cntR + 1;
+    if (cntR >= CNT_MAX) cntR = 0;
+  }
 
+  if (clickDetection(BUTTON_GREEN_PIN, &oldG)) {
+    cntG = cntG + 1;
+    if (cntG >= CNT_MAX) cntG = 0;
+  }
+
+  if (clickDetection(BUTTON_BLUE_PIN, &oldB)) {
+    cntB = cntB + 1;
+    if (cntB >= CNT_MAX) cntB = 0;
+  }
+
+  Serial.println(String(cntR) + "," + String(cntG) + "," + String(cntB));
+
+  analogWrite(LED_RED_PIN, (int)map(cntR, 0, CNT_MAX, 255, 0));
+  analogWrite(LED_GREEN_PIN, (int)map(cntG, 0, CNT_MAX, 255, 0));
+  analogWrite(LED_BLUE_PIN, (int)map(cntB, 0, CNT_MAX, 255, 0));
 }
+
+
